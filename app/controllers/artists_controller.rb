79da4fd3113d079266b9bc6b3613d7b2b076ce07
Yuriy Kulibaba <kulibkz@gmail.com>
2016-06-07 22:12:09 -0700

    init for artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,49 @@
 class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to @artist, notice: 'Pin was successfully created.'
+    else
+      render :new 
+    end
+
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    if @artist.update(artist_params)
+      redirect_to @artist, notice: 'Pin was successfully created.'
+    else
+      render :new 
+    end
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    redirect_to artists_path
+  end
+
+private
+  def artist_params
+    params.require(:artist).permit(:name, :city, :description)
+  end
+
 end
