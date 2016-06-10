aec3adb0b50109d6e18ff72dbdd89f97714f19c2
JoseRos <oxi.ros@hotmail.com>
2016-06-09 19:56:39 -0700

    added styling and installed device views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-
-  root 'artists#index'
   resources :artists
+  devise_for :users
+  root 'pages#home'
+  get 'about' => 'pages#about'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
