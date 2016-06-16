ce2c6a049de66551968c1b86eb259ffbe54e17ab
JoseRos <oxi.ros@hotmail.com>
2016-06-15 21:02:58 -0700

    added flip.js to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,22 @@
 class PagesController < ApplicationController
 skip_before_filter :authenticate_user!
+helper_method :resource_name, :resource, :devise_mapping
   def home
 
   end
 
   def about
 
+  end
+  def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
   end
 end
